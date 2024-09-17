3ad67804200668d6d19a72b8f32d56d9a41e8449
jasnmar <jasnmar@gmail.com>
2024-09-17 13:17:55 -0400

    Challenge 2 Start

--- index.js
+++ index.js
@@ -2,17 +2,11 @@ const playerGuess = 3
 const correctAnswer = 6
 
 /*
-Challenge 
-1. Refactor the if else statement to use a ternary operator.
+Challenge
+1. Now improve the functionality of this code by 
+   letting the player know if their guess was too high, 
+   too low, or exactly right.
 */
-
-// let message = ''
-// if (playerGuess === correctAnswer) {
-//     message = 'Correct!'
-// }
-// else {
-//     message = 'Wrong!'
-// }
 
 const message = playerGuess === correctAnswer ? 'Correct!' : 'Wrong!'
 
