76330275d4ada50aeeec6d5255169492c221ca14
jasnmar <jasnmar@gmail.com>
2024-09-17 17:47:18 -0400

    Preincrment Start

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-function checkUsername(userName) {
-  if (userName) {
-      console.log(userName)
-  } else {
-      throw new Error('No username provid')
-  }
+let currentTicketNumber = 0
+
+function getNextTicketNumber() {
+/*
+Challenge:
+    1. Move the increment operator to the front 
+       of the expression.
+*/
+  return currentTicketNumber++
 }
 
-checkUsername()
+// Simulating guests arriving and receiving ticket numbers
+console.log(`Guest 1, your ticket number is: ${getNextTicketNumber()}`) 
+console.log(`Guest 2, your ticket number is: ${getNextTicketNumber()}`) 
+console.log(`Guest 3, your ticket number is: ${getNextTicketNumber()}`)
