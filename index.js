16c083391505cde813c40b052e41651684520d92
jasnmar <jasnmar@gmail.com>
2024-09-17 13:12:58 -0400

    More Complicated Finished

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const exerciseTimeMins = 40
+const exerciseTimeMins = 70
 
-let message = ''
+// let message = ''
 
-if (exerciseTimeMins < 30) {
-    message = 'You need to try harder!'
-}
-else if(exerciseTimeMins < 60) {
-    message = 'Doing good!'
-}
-else {
-    message = 'Excellent!'
-}
-//const message = exerciseTimeMins < 30 ? 'You need to try harder!' : 'Doing good!'
+// if (exerciseTimeMins < 30) {
+//     message = 'You need to try harder!'
+// }
+// else if(exerciseTimeMins < 60) {
+//     message = 'Doing good!'
+// }
+// else {
+//     message = 'Excellent!'
+// }
+const message = exerciseTimeMins < 30 ? 'You need to try harder!' : exerciseTimeMins < 60 ? 'Doing good!' : 'Excellent'
 
 console.log(message)
