8cb9a231622d6edc7716c4eada9fd4f6fa3c5da7
jasnmar <jasnmar@gmail.com>
2024-09-17 13:13:50 -0400

    More Readable

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const exerciseTimeMins = 70
 // else {
 //     message = 'Excellent!'
 // }
-const message = exerciseTimeMins < 30 ? 'You need to try harder!' : exerciseTimeMins < 60 ? 'Doing good!' : 'Excellent'
+const message = exerciseTimeMins < 30 ? 'You need to try harder!' 
+  : exerciseTimeMins < 60 ? 'Doing good!' 
+  : 'Excellent'
 
 console.log(message)
