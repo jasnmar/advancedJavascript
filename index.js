4ad9b97eb3b8270a8d861b77f48635dd51888db5
jasnmar <jasnmar@gmail.com>
2024-09-17 13:31:40 -0400

    Object Destructuring Start

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
-/**
- * Price List
- * Coffee $2
- * Sandwiches $5
- * Salad $4
- * Lemon Cake $3
-*/
+const favouriteFilm = {
+  title: "Top Gun",
+  year: "1986",
+  genre: "action",
+  star: "Tom Cruise",
+  director: "Tony Scott"
+} 
 
-// For example: "You selected Salad. That will be $4"
+const title = favouriteFilm.title
+const year = favouriteFilm.year
+const genre = favouriteFilm.genre
+const star = favouriteFilm.star
+const director = favouriteFilm.director
 
-function selectItem(item) {
-  let price = 0
-/*
-Challenge:
-1.  Add the remaining price list items as cases.
-*/    
-  switch(item) {
-      case 'coffee':
-        price = 2
-        break
-      case 'sandwiches':
-        price = 5
-        break
-      case 'salad':
-        price = 4
-        break
-      case 'lemon cake':
-        price = 3
-        break
-      default:
-        return `Sorry we dont sell ${item}`
-  }
-  return `You selected ${item}. That will be $${price}`
-}
-
-console.log(selectItem('salad'))
+console.log(`My favourite film is ${title} starring ${star}. It's an ${genre} film that was directed by ${director} and released in ${year}.`)
