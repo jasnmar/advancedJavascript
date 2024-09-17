db30b2b977238d9275b80827abc557196f5c958e
jasnmar <jasnmar@gmail.com>
2024-09-17 17:26:57 -0400

    Export Challenge Start

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-function startCountdown(device) {
-  let secondsRemaining = 3
-  
-  const shutdownTimer = setInterval(function(){
-    let message =  `Your ${device} will shut down in ${secondsRemaining} seconds.` 
-    if (secondsRemaining === 0){
-      message = `Your ${device} is shutting down`
-      clearInterval(shutdownTimer)
-    } 
-    console.log(message)
-    secondsRemaining--
-  }, 1000)
-/*
-Challenge:
-1. Every 1000 milliseconds, log out: 
-  `Your device will shut down in ${secondsRemaining} seconds`.
-  Decrement 'secondsRemaining'.
-2. When 'secondsRemaining' hits zero, you should log out 
-  `Your device is shutting down`. 
-  At this point, the setInterval should be cleared. What method 
-  can you use to do that? (We used clearTimeout to cancel setTimeout.)
-*/
-}
-startCountdown('Macbook')
+import { interplanetaryDestinationsArr as destinations, shortSpaceTripsArr } from '/data.js'
+
+console.log(shortSpaceTripsArr)
