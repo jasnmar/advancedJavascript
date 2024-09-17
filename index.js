c06ed64b0ec4e7458b361e6e9746e1571e2cf72c
jasnmar <jasnmar@gmail.com>
2024-09-17 13:47:06 -0400

    Challenge 2 start

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
+function logAnswer() {
+  console.log(`The answer is ?????? of course! If you got that right, giver yourself ?????? points.`)
+}
+
 console.log('What is the capital of Peru?')
-
-setTimeout(function(){
-    console.log('Lima!')
-}, 3000)
-setTimeout(function(){
-  console.log('Ready for next question?')
-}, 6000)
-
 /*
 Challenge:
-1. Make the third console.log fire 3 seconds after 
-   the answer appears.
+  1. After a 3 second delay, call the 'logAnswer' function.
+  2. Make sure 'logAnswer' has all the info it needs. 
+     The answer is 'Lima' and it's 10 points for getting it right. 
 */
