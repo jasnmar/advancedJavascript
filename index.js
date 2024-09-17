86db68d2bc0f9378f3e0720da97b3cd43922110d
jasnmar <jasnmar@gmail.com>
2024-09-17 18:02:03 -0400

    Starting Point

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-// Challenge:
-// 1. Predict what will be logged to the console.
+/*
+App requirements:
+ - The app should display the name, symbol, and 
+   price of the stock, with a timestamp as per the 
+   screenshot. 
+ - The triangle compares the current stock price to 
+   its previous price. If the price has increased, it 
+   should be a green triangle pointing up, if the price 
+   has decreased it should be a red triangle pointing 
+   down, and if there has been no change it should be a 
+   grey triangle pointing to the right.
+ - The price should update every 1.5 seconds. 
+*/
 
-function getWeather() {
-  return "Today's weather is warm and sunny"
+/*
+Challenge:
+  1. Find a way to get fresh stock data every 1.5 seconds.
+  2. Call the renderStockTicker function with the fresh data.
+  3. Add logic to renderStockTicker to display the correct 
+     information.
+  ⚠️ You will need to write code here in index.js and in
+   fakeStockAPI.js.
+*/
+
+function renderStockTicker(stockData) {
+  const stockDisplayName = document.getElementById('name')
+  const stockDisplaySymbol = document.getElementById('symbol')
+  const stockDisplayPrice = document.getElementById('price')
+  const stockDisplayPriceIcon = document.getElementById('price-icon')
+  const stockDisplayTime = document.getElementById('time')
 }
 
-console.log(getWeather()) // What will this log?
-
-console.log(getNews()) // What will this log?
-
-function getNews() {
-  return "A new swimming pool has opened in the town centre..."
-}
-
-console.log(trafficInfo) // What will this log?
-
-let trafficInfo = 'All roads are busy right now'
