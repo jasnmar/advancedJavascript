42b9a54c7e5c34040abf00f650bfc922b911e6fa
jasnmar <jasnmar@gmail.com>
2024-09-19 12:54:24 -0400

    Filter Start

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-import { playlistArr } from '/playlist.js'
+const ages = [1, 5, 9, 23, 56, 10, 47, 70, 10, 19, 23, 18]
 
+const adults = ages.filter(function(age){
 /*
-Challenge
-1. Refactor the code below to use .map() 
-   instead of the for loop.
-   ⚠️ Don't worry about the commas for now.
+Challenge:
+1. Get rid of the if statement and reduce the 
+   logic in this function to just one line 
+   of code.
 */
-const pl2 = []
- playlistArr.forEach(song => {
-  pl2.push(
-    `<section class="card">
-    <div class="card-start">
-        <img src="/images/${song.albumArt}">
-    </div>
-        <div class="card-mid">
-            <h4 class="card-title">${song.title}</h4>
-            <p class="card-artist">${song.artist}</p>
-        </div>
-    <div class="card-end">
-        <p class="card-menu">...</p>
-    </div>
-</section>
-    `
-  )
-});
-console.log('pl2: ', pl2)
-console.log('playlistArr: ', playlistArr)
-document.getElementById('container').innerHTML = pl2.join('')
+    if (age >= 18){
+        return true
+    }
+})
+
+console.log(adults)
