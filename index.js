800ff86f6f656ec0e2511afebce04fe8ec781904
jasnmar <jasnmar@gmail.com>
2024-09-19 13:37:19 -0400

    ForEach Start

--- index.js
+++ index.js
@@ -9,29 +9,28 @@ const characters = [
       emoji: '🧙',
       powers: ['magic', 'invisibility', 'necromancy'],
   },
-  {
+  { 
       title: 'Ogre',
       emoji: '👹',
       powers: ['power', 'stamina', 'shapeshifting'],
-  },
-  {
+  },  
+  { 
       title: 'Unicorn',
       emoji: '🦄',
       powers: [ 'flight', 'power', 'purity'],
   }
 ]
 
-for (let character of characters){
+// for (let character of characters){
+//     console.log(character)
+// }
+
+characters.forEach(function(character){
   console.log(character.powers)
-  for (let power of character.powers) {
-    console.log(power)
-  }
 /*
 Challenge:
-1. Nest a for of inside this for of to iterate over 
- the powers array for each character. Log out each 
- power.
+1. Nest a forEach to log out each individual
+ power in each characters powers array.
 */
-}
-
+})
 
