a0f4f4b7ea31f2453f9448ab16d8508de65705d9
jasnmar <jasnmar@gmail.com>
2024-09-19 14:22:27 -0400

    Replace

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const sentence = "i went to Australia and i saw a shark"
 
+
+const nsent = sentence.replaceAll("i ", "I ")
+
+console.log(nsent)
 /*
 Challenge:
     1. Change the lowercase i’s to uppercase.
