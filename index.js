8e941c012344da3b1ce2acfa459e5fc4fea56cc6
jasnmar <jasnmar@gmail.com>
2024-09-19 12:25:59 -0400

    Map Solved

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-// Challenge:
-// 1. Predict what will be logged to the console.
+import { playlistArr } from '/playlist.js'
 
-function getWeather() {
-  return "Today's weather is warm and sunny"
-}
+/*
+Challenge
+1. Refactor the code below to use .map() 
+   instead of the for loop.
+   ⚠️ Don't worry about the commas for now.
+*/
+const pl2 = playlistArr.map((song) => {
+  return (
+    `<section class="card">
+    <div class="card-start">
+        <img src="/images/${song.albumArt}">
+    </div>
+        <div class="card-mid">
+            <h4 class="card-title">${song.title}</h4>
+            <p class="card-artist">${song.artist}</p>
+        </div>
+    <div class="card-end">
+        <p class="card-menu">...</p>
+    </div>
+</section>
+    `
+  )
+})
 
-console.log(getWeather()) // What will this log?
 
-console.log(getNews()) // What will this log?
-
-function getNews() {
-  return "A new swimming pool has opened in the town centre..."
-}
-
-console.log(trafficInfo) // What will this log?
-
-let trafficInfo = 'All roads are busy right now'
+document.getElementById('container').innerHTML = pl2
