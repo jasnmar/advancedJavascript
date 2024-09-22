accd5e5ff47bd844621ee1197e29d08a46dfbcdc
jasnmar <jasnmar@gmail.com>
2024-09-22 11:41:34 -0400

    Arrow Challenge 3 Solution

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const distanceTraveledMiles = [267, 345, 234, 190, 299]
 
-const distanceTraveledKm = distanceTraveledMiles.map(function(distance){
-    return Math.round(distance * 1.6)
-})
+const distanceTraveledKm = distanceTraveledMiles.map(distance => Math.round(distance * 1.6)
+)
 
 console.log(distanceTraveledKm)
 
