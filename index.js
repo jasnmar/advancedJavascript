84c7fc7d6b19ea3e8d62a600f3f9dd589f11f10d
jasnmar <jasnmar@gmail.com>
2024-09-22 11:33:37 -0400

    Arrow Challenge 2 start

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const speedWarning= speed => `You are going at ${speed} mph!`
+const speedWarning = speed => `You are going at ${speed} mph!`
 
-
-console.log(speedWarning(40))
+console.log(speedWarning(30, 40))
 
 /*
 Challenge
-1. Refactor this function to use an arrow function.
-2. Make sure you write the least code possible.
+1. Refactor this function so it only warns drivers 
+   who are going over the speed limit.
+2. The function now needs to take in two parameters. 
+   The first is the speed limit, the second is the 
+   driver's actual speed.
 */
