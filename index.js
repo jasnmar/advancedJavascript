b03c8753e26552dca6ec1c097ea1d3aa04964dad
jasnmar <jasnmar@gmail.com>
2024-09-22 11:51:46 -0400

    Inline Arrow Challenge Solution

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import { itemsBoughtArr } from '/itemsBoughtArr.js'
 
-function calculateTotalCost(itemsBoughtArr){
+const calculateTotalCost = itemsBoughtArr => itemsBoughtArr.reduce((acc, val) => acc + val.priceUSD, 0)
+
+console.log(calculateTotalCost(itemsBoughtArr))
+
 /*
 Challenge:
 1. Use the reduce method to calculate the total 
    cost of items which have been bought.
 */
-    const total = 
-    return total
-}
-
-console.log(calculateTotalCost(itemsBoughtArr))
