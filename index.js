d2e736be0edf1d259dd8bc53567d5f0a109cb145
jasnmar <jasnmar@gmail.com>
2024-09-22 11:44:44 -0400

    Inline Arrow Challenge

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const distanceTraveledMiles = [267, 345, 234, 190, 299]
+import { itemsBoughtArr } from '/itemsBoughtArr.js'
 
-const distanceTraveledKm = distanceTraveledMiles.map(distance => Math.round(distance * 1.6)
-)
-
-console.log(distanceTraveledKm)
-
+function calculateTotalCost(itemsBoughtArr){
 /*
-Challenge
-1. Refactor this .map method so the inline function is
-   an arrow function. 
-2. Write the least amount of code possible.
+Challenge:
+1. Use the reduce method to calculate the total 
+   cost of items which have been bought.
 */
+    const total = 
+    return total
+}
+
+console.log(calculateTotalCost(itemsBoughtArr))
