5fde64a9ed958390ff6befc5d26827b1ca06838c
jasnmar <jasnmar@gmail.com>
2024-09-22 12:22:40 -0400

    Rest finish

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-function setPermissionLevel(permissionLevel, name1, name2, name3) {
-  console.log(`${name1} now has ${permissionLevel} level access.`)
-  console.log(`${name2} now has ${permissionLevel} level access.`)
-  console.log(`${name3} now has ${permissionLevel} level access.`)
+function setPermissionLevel(permissionLevel, ...rest) {
+  rest.forEach((name)=> console.log(`${name} now has ${permissionLevel} level access.`))
 }
-
 setPermissionLevel('admin', 'Dave', 'Sally', 'Mike')
