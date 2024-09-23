64ca9f04e53f1a6beebf58af268e05872ad097d4
jasnmar <jasnmar@gmail.com>
2024-09-23 14:39:06 -0400

    Complete

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ Challenge:
 ⚠️ Remember to handle all errors!
 */
  
+
+try {
+    const response = await fetch('https://apis.scrimba.com/jsonplaceholder/posts')
+    if (!response.ok)
+        throw new Error("Error with the API");
+    const data = await response.json()
+    data.forEach(post => {
+        console.log('post: ', post)
+    });
+} catch(err) {
+    console.log('err: ', err)
+}
