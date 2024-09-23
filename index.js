8d37897824581a614c9c6a334d3d8e4759db2abf
jasnmar <jasnmar@gmail.com>
2024-09-23 14:47:10 -0400

    Complete

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 try {
-    const response = await fetch('https://apis.scrimba.com/jsonplaceholder/posts', {method: 'POST'})
+    const response = await fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: "Holiday Nightmares",
+            body: "When I was kidnapped in Scotland…",
+            userId: 3,
+        })
+    })
     if (!response.ok) {
         throw new Error('There was a problem with the API')
     }
