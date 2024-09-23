d463c06d0f194cbfda4ee91244257eb37efae180
jasnmar <jasnmar@gmail.com>
2024-09-23 14:15:27 -0400

    Challenge Complete

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ Challenge:
     🛟 hint.md for help!
 */
 
+const response = await fetch('https://apis.scrimba.com/bored/api/activity')
+const data = await response.json()
+console.log('data: ', data.activity)
