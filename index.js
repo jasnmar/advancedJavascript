c89164b61d78e6203935eef0877466c8e08db248
jasnmar <jasnmar@gmail.com>
2024-09-23 16:05:09 -0400

    Super Challenge

--- index.js
+++ index.js
@@ -1,60 +1,50 @@
-function uploadFile() {
+function getImagePromise(url) {
     return new Promise((resolve, reject) => {
-        console.log('Step 1: Uploading file...')
         setTimeout(() => {
-            resolve() // Call the next step after 1 second
-        }, 1000)
+            const img = new Image()
+            img.src = url
+            img.alt = 'scenic image'
+            img.addEventListener('load', () => resolve(img))
+            img.addEventListener('error', () => reject(new Error(`Failed to load image: ${url}`)))
+        }, 500)
     })
 }
 
-function processFile() {
-    return new Promise((resolve, reject) => {
-        console.log('Step 2: Processing file...')
-        setTimeout(() => {
-            resolve() // Call the next step after 1 second
-        }, 1000)
-    })
-}
+const images = [
+    'https://scrimba.com/links/advancedjs-resources-images-scenic1',
+    'https://scrimba.com/links/advancedjs-resources-images-scenic2',
+    'https://scrimba.com/links/advancedjs-resources-images-scenic3'
+]
 
-function notifyUser() {
-    return new Promise((resolve, reject) => {
-        console.log('Step 3: Notifying user...')
-        setTimeout(() => {
-            resolve() // Call the next step after 1 second
-        }, 1000)
-    })
-}
+async function preloadImages(imageUrlsArr) {
+    const imgContainer = document.getElementById('img-container')
+    const uploadContainer = document.getElementById('upload-container')
+    try {
+        const promArray = imageUrlsArr.map((imageUrl) => {
+            return getImagePromise(imageUrl)
+        })
+        const result = await Promise.all(promArray)
+        console.log("All images loaded successfully")
+        uploadContainer.classList.add('hidden')
+        result.forEach((image) => {
+            imgContainer.appendChild(image)
+        })
+    } catch (err) {
+        console.log('err: ', err)
+    }
 
-/* 
+/*
 Challenge:
-1. Await these promises in order in a try/catch block and 
-   when they are done, log 'All steps completed!'.
+  1. Create an array of promises using getImagePromise.
+  2. Save the results of calling all of those promises 
+     in one go to a const 'results'.
+  3. If the promises resolve: 
+     - log "All images loaded successfully!".
+     - hide 'uploadContainer'
+     - iterate over the results and render them to imgContainer.
+  4. If the promises reject:
+     - catch and log the error.
 */
-try {
-    await uploadFile()
-    await processFile()
-    await notifyUser()
-    console.log("All steps complete")
-
-} catch (err) {
-    console.log('err: ', err)
 }
-// expected output:
 
-// Step 1: Uploading file...
-// Step 2: Processing file...
-// Step 3: Notifying user...
-// All steps completed!
-
-
-
-
-// uploadFile(() =>
-//     processFile(() =>
-//         notifyUser(
-//             () => console.log('All steps completed!')
-//         )
-//     )
-// )
-
-
+document.getElementById('submit-imgs').addEventListener('click', () => preloadImages(images))
