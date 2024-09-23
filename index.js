9e27eb570adb8083711061b230c8382951689375
jasnmar <jasnmar@gmail.com>
2024-09-23 14:38:56 -0400

    NextLevel Start

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-// fetch('https://api.scrimba.com/dog.ceo/api/breeds/image/random')
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-//     .catch(err => {
-//         console.log(err)
-//         // update the DOM to warn the user
-//         // access an alternative API
-//     })
-//     .finally(()=> console.log('The operation completed.'))  
-
-
 /*
+Base URL: https://apis.scrimba.com/jsonplaceholder
+Endpoint: /posts
 Challenge:
-1. Convert the above code to use async/await, handle errors with “try/catch” blocks, and add a “finally” block.
-
+1. Make a fetch request to get all of the available posts.
+⚠️ Remember to handle all errors!
 */
-
-try {
-    const response = await fetch('https://apis.scrimba.com/dog.ceo/api/breeds/image/random')
-    const data = await response.json()
-    console.log('data: ', data)
-} catch(err) {
-    console.log(err)
-} finally {
-    console.log('The operation completed.')
-}
+ 
