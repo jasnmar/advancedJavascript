c55ba414e4bc58418cb9497adca820a6a70cbe0c
jasnmar <jasnmar@gmail.com>
2024-09-23 16:21:34 -0400

    Short Circuit Or Challenge Start

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// Challenge:
-// 1. Predict what will be logged to the console.
+const jobHunter = {
+    name: 'Tom Chant',
+    username: 'TChant44',
+    workLocation: 'Europe',
+    }
+    
+/*
+Challenge:
+1. Make it so that if the jobHunter object does not have 
+   a name property, the username is used instead.
+*/
+    
+// Complete this line of code 👇
+const jobHunterName = 
 
-function getWeather() {
-  return "Today's weather is warm and sunny"
-}
-
-console.log(getWeather()) // What will this log?
-
-console.log(getNews()) // What will this log?
-
-function getNews() {
-  return "A new swimming pool has opened in the town centre..."
-}
-
-console.log(trafficInfo) // What will this log?
-
-let trafficInfo = 'All roads are busy right now'
+console.log(`Hey ${jobHunterName}!`)
