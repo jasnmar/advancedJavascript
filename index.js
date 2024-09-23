84956b804ba1ffe759be3ec77bd5b5d6c3c00dd8
jasnmar <jasnmar@gmail.com>
2024-09-23 15:03:39 -0400

    Promise Intro

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-try {
-    const response = await fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            title: "Holiday Nightmares",
-            body: "When I was kidnapped in Scotland…",
-            userId: 3,
-        })
-    })
-    if (!response.ok) {
-        throw new Error('There was a problem with the API')
+
+const promise = new Promise((resolve, reject)=> {
+    const success = Math.random() > 0.5
+    if (success) {
+        resolve('Operation Successful')
+    } else {
+        reject('Operation failed')
     }
-    const data = await response.json()
-    console.log(data)
-} catch(err) {
-    console.log(err)
-}  
-/*
-Challenge:
-1. Add a headers object, setting the "Content-Type" to "application/json".
-*/
+})
+
+try {
+    const response = await promise
+    console.log('promise: ', promise)
+} catch (err) {
+    console.log('err: ', err)
+}
