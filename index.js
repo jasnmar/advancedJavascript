684e3799441ba949c6f91f45cbb980aaeafadc7e
jasnmar <jasnmar@gmail.com>
2024-09-23 14:51:03 -0400

    Header Start

--- index.js
+++ index.js
@@ -15,15 +15,7 @@ try {
 } catch(err) {
     console.log(err)
 }  
-
 /*
 Challenge:
-1. Using the code snippet in the slide, add a body 
-   property to the object we are passing with the 
-   fetch request. I want you to create a new post 
-   with the title “Holiday Nightmares” and the body 
-   “When I was kidnapped in Scotland…” 
-   
-   In the console, you should see and object with an 
-   ID, for example: {id: 101}
+1. Add a headers object, setting the "Content-Type" to "application/json".
 */
