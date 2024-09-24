c88162e59ba161089204f6e5918c09cf68b5afcc
jasnmar <jasnmar@gmail.com>
2024-09-24 14:04:43 -0400

    Object Assign Challenge Start

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-const averageSharePriceByMonthQ1 = [109.6, 103.3, 89.4]
-const averageSharePriceByMonthQ2 = [109.3, 126.1, 103.3]
-const averageSharePriceByMonthQ3 = [120.8, 102.3, 106.8]
-const averageSharePriceByMonthQ4 = [110.9, 119.8, 113.7]
+const studentDetails = {
+    firstName: 'janaka',
+    lastName: 'siriwardena',
+    age: 28,
+    country: 'sri lanka',
+    email: 'j.siri@totalinternet.com',
+    discordUsername: 'JS1',
+    }  
+    
+const studentDetailsCopy = {}
 
-function findPriceExtremes(arr){
-/*
-Challenge:
-2. Find the highest number from the array 
-   and store it in the const 'highest'. 
-3. Find the lowest number from the array 
-   and store it in the const 'lowest'. 
-*/
-    const highest = Math.max(...arr)
-    const lowest = Math.min(...arr)
-    console.log(`The highest average share price was ${highest}`)
-    console.log(`The lowest average share price was ${lowest}`)
-}
+Object.assign(studentDetailsCopy, studentDetails)
+
+console.log(studentDetailsCopy)
 
 /*
 Challenge:
-1. Call this function with one array holding 
-   all of the data from the 4 arrays above.
+    1. Prove that what we have created here is a shallow copy.
 */
-const allPrices = [...averageSharePriceByMonthQ1, ...averageSharePriceByMonthQ2, ...averageSharePriceByMonthQ3, ...averageSharePriceByMonthQ4]
-findPriceExtremes(allPrices)
