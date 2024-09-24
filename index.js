667b0625de9df8ecffe0e1af4286e8a05d91df21
jasnmar <jasnmar@gmail.com>
2024-09-24 13:07:43 -0400

    Challenge 2 start

--- index.js
+++ index.js
@@ -4,31 +4,11 @@ const books = {
     "b003": { title: "The Great Gatsby", price: 12.49, isAvailable: true },
     "b004": { title: "Moby Dick", price: 22.50, isAvailable: false }
   }
-
-  const bookKeys = Object.keys(books)
-  console.log('bookKeys: ', bookKeys)
   
-  bookKeys.forEach(key => console.log(key))
-  bookKeys.forEach(key => console.log(books[key].title))
   /*
   Challenge:
-    1. Use Object.keys to get an array of keys. 
-       You can store it in a const 'bookKeys'. 
-    2. Iterate over bookKeys twice.
-       A) First log each individual key in the array.
-       B) Then log only the book titles.
+    1. Create an array of values using Object.values
+    2. Iterate over it and log out each book price.
   */
   
-  
-  
-  /*
-  Output: b001
-          b002
-          b003
-          b004
-  Output: To Kill a Mockingbird
-          1984
-          The Great Gatsby
-          Moby Dick
-  */
-  
+  const bookValues = 
