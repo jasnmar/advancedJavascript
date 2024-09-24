8df332c2a59034519c8fb6570135473b45dc86bc
jasnmar <jasnmar@gmail.com>
2024-09-24 15:42:43 -0400

    Polymorphism constructor start

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-/*
-Challenge:
-Rebuild this constructor function as a class.
-*/
-class Character {
-    constructor(name) {
-        this.name = name
-        this.collectedItemsArr = []
-    }
-    addItem(item) {
-        this.collectedItemsArr.push(item)
-        console.log(`${this.name} now has: ${this.collectedItemsArr.join(", ")}.`)
+function Event(name, location, date) {
+    this.name = name
+    this.location = location
+    this.date = date
+    this.getDetails = function() {
+        return `Event: ${this.name}, Location: ${this.location}, Date: ${this.date}`
     }
 }
 
-const wizard = new Character('Merlin')
-wizard.addItem('a wand')
-wizard.addItem('potion')
+
+
+/*
+Challenge:
+    1. Remove getDetails from the Event constructor's body 
+       and add it to the Event constructor’s prototype. 
+*/
+
+Event.prototype.cancelEvent = function() {
+    return 'Event Cancelled'
+}
+
+function Concert(name, location, date, headliner) {
+    Event.call(this, name, location, date)
+    this.headliner = headliner
+    // this.getDetails = function() {
+        
+    // }
+}
+
+Concert.prototype = Object.create(Event.prototype)
+Concert.prototype.constructor = Concert
+
+const concert = new Concert("Summer Beats", "City Stadium", "2024-07-15", "The Electrons")
+const concert2 = new Concert("Concert Two", "Highlands Park", "2024-08-08", "Dave Notes")
+const concert3 = new Concert("Concert Three", "Highlands Park", "2024-08-08", "Dave Notes")
+console.log(concert)
+console.log(concert2)
+console.log(concert3)
