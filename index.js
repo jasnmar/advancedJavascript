1d6acdc61ade7379b7d7c0b4d6f5fcef489b57df
jasnmar <jasnmar@gmail.com>
2024-09-24 13:58:23 -0400

    Spread Operator Challenge Start

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-const user1 = {
-    username: "rpchan",
-    subscriptionLevel: "bronze",
-  }
-  
-  const user2 = {
-    username: "bcstevens",
-    subscriptionLevel: "silver",
-    accessPremiumFeature: true
-  }
-  
-  function canAccessPremiumFeature(userObj, prop) {
+const averageSharePriceByMonthQ1 = [109.6, 103.3, 89.4]
+const averageSharePriceByMonthQ2 = [109.3, 126.1, 103.3]
+const averageSharePriceByMonthQ3 = [120.8, 102.3, 106.8]
+const averageSharePriceByMonthQ4 = [110.9, 119.8, 113.7]
 
-    if(Object.hasOwn(userObj, prop)) {
-        return userObj[prop]
-    }
-    return false
-  /*
-  Challenge:
-  1. Write logic to check if the object has the property. 
-     Do this challenge twice, once with hasOwn and once 
-     with hasOwnProperty. All the function need do is return
-     a boolean. 
-     Bonus: use short circuiting to only return true if 
-     'accessPremiumFeature' both exists AND is true.
-  */ 
-  }
-  
-  console.log(canAccessPremiumFeature(user1, 'accessPremiumFeature'))
-  console.log(canAccessPremiumFeature(user2, 'accessPremiumFeature')) 
-  
-  
+function findPriceExtremes(arr){
+/*
+Challenge:
+2. Find the highest number from the array 
+   and store it in the const 'highest'. 
+3. Find the lowest number from the array 
+   and store it in the const 'lowest'. 
+*/
+    const highest = //???
+    const lowest = //???
+    console.log(`The highest average share price was ${highest}`)
+    console.log(`The lowest average share price was ${lowest}`)
+}
+
+/*
+Challenge:
+1. Call this function with one array holding 
+   all of the data from the 4 arrays above.
+*/
+findPriceExtremes()
