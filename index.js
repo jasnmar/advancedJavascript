716f2364f1373c77072f4c4ad2295ff993309845
jasnmar <jasnmar@gmail.com>
2024-09-24 15:17:42 -0400

    Completed

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 Challenge:
 Rebuild this constructor function as a class.
 */
-
-
-function Character(name) {
-    this.name = name
-    this.collectedItemsArr = []
-    this.addItem = function (item) {
+class Character {
+    constructor(name) {
+        this.name = name
+        this.collectedItemsArr = []
+    }
+    addItem(item) {
         this.collectedItemsArr.push(item)
-        console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`)
+        console.log(`${this.name} now has: ${this.collectedItemsArr.join(", ")}.`)
     }
 }
 
 const wizard = new Character('Merlin')
 wizard.addItem('a wand')
+wizard.addItem('potion')
