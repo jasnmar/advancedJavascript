0ae781470c40f2e92a84e9a9faef933fb8624404
jasnmar <jasnmar@gmail.com>
2024-09-25 14:08:53 -0400

    Complete

--- index.js
+++ index.js
@@ -6,19 +6,27 @@ const athlete4 = { name: 'Judy', averageTime10KmMins: 66.0 }
 /* Challenge */
 
 /* 1. Create a map object "athletes" to store the athletes. */
+const athletes = new Map()
 
 function addAthlete(athlete, time) {
+    athletes.set(athlete, time)
+    
 /* 2. This function should add athletes to the "athletes" map. */
 }
 
 function getSummary(){
+    console.log('athletes: ', athletes)
+    athletes.forEach((value, key) => {
+        console.log(`${key.name}'s average time is ${key.averageTime10KmMins} but today ${key.name} achieved ${value}`)
+    })
+        
+    }
 /* This function should make the following appear in the console */
     
 //Alice's average time is 58.3 but today Alice achieved 57.3
 //Dave's average time is 53.2 but today Dave achieved 61.1
 //Micky's average time is 64.5 but today Micky achieved 59.9
 //Judy's average time is 66 but today Judy achieved 61.6
-}
 
 addAthlete(athlete1, 57.3)
 addAthlete(athlete2, 61.1)
