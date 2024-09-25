788912e3643762eec197ff21066bdc95e0e2ead1
jasnmar <jasnmar@gmail.com>
2024-09-25 14:41:38 -0400

    Complete

--- index.js
+++ index.js
@@ -4,10 +4,8 @@
        it async and how to pass in parameters.
 */
 
-async function displayWeather(country) {
+(async function displayWeather(country) {
     const weather = await fetch('/geo.json')
     const weatherObj = await weather.json()
     console.log(`The weather in the ${country} today is ${weatherObj[country]}`)    
-}
-
-displayWeather('UK')
+})('Japan')
