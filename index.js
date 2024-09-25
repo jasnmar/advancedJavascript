06a13d21f3ca3ca64f268c4e9c2299cb51a05a3f
jasnmar <jasnmar@gmail.com>
2024-09-25 12:09:33 -0400

    Complete

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ class Holiday {
         4. Test! 
     */
         #destination
+        #price
         constructor(destination, price) {
             this.#destination = destination
-            this.price = price
+            this.#price = price
         }
     
         get destination() {
             return this.#destination
+        }
+        get price() {
+            return `$${this.#price.toFixed(2)}`
         }
     
         set destination(newDestination) {
@@ -25,7 +29,14 @@ class Holiday {
             }
             this.#destination = newDestination
         }
+        set price(newPrice) {
+            if (typeof newPrice !== 'number' || newPrice < 0 ){
+                throw new Error('Invalid Price')
+            }
+            this.#price = newPrice
+        }
     }
     
     const safari = new Holiday('Kenya', 1000)
+    safari.price = 2000
     console.log(safari.price)
