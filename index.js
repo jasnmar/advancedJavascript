5de0e5f97b21348a522876ffbb118e5ae448598b
jasnmar <jasnmar@gmail.com>
2024-09-26 11:52:33 -0400

    Complete

--- index.js
+++ index.js
@@ -10,15 +10,18 @@ Challenge:
     🛟 hint.md for help.
 */
 const logMessage = (level, message) => `[${level.toUpperCase()}] ${message}`
+const logger = (level) => (message) => `[${level.toUpperCase()}] ${message}`
 
 // Log messages with "info" level
-console.log(logMessage("info", "Application started")) // Output: "[INFO] Application started"
-console.log(logMessage("info", "User logged in")) // Output: "[INFO] User logged in"
-
+const infoLogger = logger("info")
+console.log(infoLogger("Application started"))
+console.log(infoLogger("User logged in"))
 // Log messages with "warn" level
-console.log(logMessage("warn", "Low disk space")) // Output: "[WARN] Low disk space"
-console.log(logMessage("warn", "High memory usage")) // Output: "[WARN] High memory usage"
+const warnLogger= logger("warn")
+console.log(warnLogger("Low disk space")) // Output: "[WARN] Low disk space"
+console.log(warnLogger("High memory usage")) // Output: "[WARN] High memory usage"
 
 // Log messages with "error" level
-console.log(logMessage("error", "Unhandled exception")) // Output: "[ERROR] Unhandled exception"
-console.log(logMessage("error", "Failed to save file")) // Output: "[ERROR] Failed to save file"
+const errorLogger = logger("error")
+console.log(errorLogger("Unhandled exception"))
+console.log(errorLogger("Failed to save file"))
