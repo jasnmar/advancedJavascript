60130609ce5ce0309abf4864159a3a8b4e7d5ec5
jasnmar <jasnmar@gmail.com>
2024-09-26 12:08:50 -0400

    Throttling Challenge Complete 1

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-/*
-Challenge:
-    1. Curry this function!
-    Set up three partially applied functions called 'infoLogger', 
-    'warnLogger', and 'errorLogger'. 
-    The partially applied functions should have their level ('info', 
-    'warn', 'error'). 
-    You should be able to call these functions and pass in a message.
-    E.g. console.log(warnLogger("Low disk space")) would log: "[WARN] Low disk space".
-    🛟 hint.md for help.
-*/
-const logMessage = (level, message) => `[${level.toUpperCase()}] ${message}`
-const logger = (level) => (message) => `[${level.toUpperCase()}] ${message}`
+function handleResize(e){
+    console.log('resize happened on event: ' + e)
+}
 
-// Log messages with "info" level
-const infoLogger = logger("info")
-console.log(infoLogger("Application started"))
-console.log(infoLogger("User logged in"))
-// Log messages with "warn" level
-const warnLogger= logger("warn")
-console.log(warnLogger("Low disk space")) // Output: "[WARN] Low disk space"
-console.log(warnLogger("High memory usage")) // Output: "[WARN] High memory usage"
 
-// Log messages with "error" level
-const errorLogger = logger("error")
-console.log(errorLogger("Unhandled exception"))
-console.log(errorLogger("Failed to save file"))
+function throttle(func, delay) {
+    let throttleTimeout = null
+    return function(e) {
+        if(!throttleTimeout) {
+            func(e)
+            throttleTimeout = setTimeout(() => {
+                throttleTimeout = null
+            }, delay)
+        } 
+    }
+}
+const throttleHandleResize = throttle(handleResize, 500)
+window.addEventListener('resize', throttleHandleResize)
